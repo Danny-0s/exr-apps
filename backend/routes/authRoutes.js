0a9d1237a64a7378e4331b38454d92e47f0ebae8
NEPA <danielshrestha505@gmsil.com>
2026-02-21 03:01:17 +0900

    rename User to UserModel

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -2,8 +2,6 @@ import express from "express";
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 import User from "../models/User.js";
-import { userAuth } from "../middleware/userAuth.js";
-
 const router = express.Router();
 
 /* ======================================================

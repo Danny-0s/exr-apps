04f9198ceab9b97457c0a6b1df07af3eaa096600
NEPA <danielshrestha505@gmsil.com>
2026-02-22 03:55:58 +0900

    fix production backend url

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,6 +1,6 @@
 const API_BASE_URL =
     import.meta.env.MODE === "development"
         ? "http://localhost:4242"
-        : "https://PASTE-YOUR-BACKEND-URL-HERE";
+        : "https://exr-apps-backend.onrender.com";
 
 export default API_BASE_URL;
